5a8b40259a79ed558e83a3a6bab4f845a73578db
H.C. Lee <lihaicheng_work@163.com>
2018-12-26 13:40:29 +0800

    注释

--- scripts/ClassTester.sh
+++ scripts/ClassTester.sh
@@ -16,11 +16,16 @@ testedJarEnvTypes="${bmBase}/${bm}/env_types.txt"
 
 # ClassTester arguments:
 #
+# 0:被测试的类
 # 0: the class under test (CUT)
+# 1:带有辅助类型名称的文件
 # 1: file with names of auxiliary types
 # 2: random seed
+# 3: max。nb试图生成后缀(即它应该运行多长时间)
 # 3: max. nb of tries to generate suffixes (i.e. how long it should run)
+# 4:结果文件(只有在发现线程安全违规时才写)
 # 4: result file (only written when a thread safety violation is found)
+# 5:是否在每次测试之前重置静态状态(只有在使用ClinitRewriter检测类时才有效)
 # 5: whether to reset static state before each test (only works when classes have been instrumented with ClinitRewriter)
 
 seed=3
@@ -29,5 +34,6 @@ maxSuffixGenTries=100
 cmd="java -cp ${contegeLibs}:${contege}:${contegeOwnLibs}:${testedJar}:${testedJarLibs} contege.ClassTester org.jfree.data.XYSeries ${testedJarEnvTypes} ${seed} ${maxSuffixGenTries} result.out false"
 
 echo "${cmd}\n"
+# 解析并执行
 eval ${cmd}
 cat result.out
